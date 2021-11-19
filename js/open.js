319a4d67353727cfec4e47f8ff376ae24ce03af3
lhs2506 <80104419+lhs2506@users.noreply.github.com>
2021-11-19 21:34:41 +0900

    Add files via upload

---
+++ js/open.js
@@ -0,0 +1,43 @@
+﻿ $(function(){
+
+        //1번째 이미지
+        $('.btn1').click(function(){
+          window.open("img/1.jpg","blank","width=1024, height=768")
+        });
+
+
+
+        //2번째 이미지
+        $('.btn2').click(function(){
+          window.open("img/2.jpg","blank","width=768, height=1024")
+        });
+
+
+
+        //3번째 이미지
+        $('.btn3').click(function(){
+          window.open("img/3.jpg","blank","width=1024, height=768")
+        });
+
+
+        //4번째 이미지
+        $('.btn4').click(function(){
+          window.open("img/4.jpg","blank","width=768, height=1024")
+        });
+
+
+       //5번째 이미지
+        $('.btn5').click(function(){
+          window.open("img/5.jpg","blank","width=1024, height=768")
+        });
+      
+
+      //6번째 이미지
+        $('.btn6').click(function(){
+          window.open("img/6.jpg","blank","width=768, height=1024")
+        });
+
+
+
+
+   });
